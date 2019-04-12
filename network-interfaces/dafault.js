388352ff55af27c6838c5f715183677c9710a859
dadmor <dadmor@gmail.com>
2019-04-12 16:25:25 +0200

    first commit

---
+++ network-interfaces/dafault.js
@@ -0,0 +1,17 @@
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpEndpoint = 'http://localhost:4000';
+
+export default (ctx) => {
+  // compose your Links here for the current client
+  const appLink = new HttpLink({ uri: httpEndpoint })
+  // here you can place your middleware. ctx has the context forwarded from Nuxt
+  // ...
+  // return the an object with additional apollo-client options
+  return {
+    uri: httpEndpoint,
+    cache: new InMemoryCache(),
+    dataIdFromObject: o => o.id
+  }
+}
